cb91421485df57a03c0deb1155bea9f5ddea2cfc
jacopo_farina <jacopo.farina@spindox.it>
2016-04-24 12:09:32 +0200

    First working version

--- neo4j_markov_chains/__init__.py
+++ neo4j_markov_chains/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Jacopo Farina'
-__email__ = 'jacopo1.farina@gmail.com'
+__email__ = 'jacopo1.farina at gmail.com'
 __version__ = '0.1.0'
+
+from .neo4j_markov_chains import Neo4jMarkovChain
